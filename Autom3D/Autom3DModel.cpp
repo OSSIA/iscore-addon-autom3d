437515aa14a4087581ae1daa94be0d85f6fe2496
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-09-30 11:10:38 +0200

    update with i-score

--- Autom3D/Autom3DModel.cpp
+++ Autom3D/Autom3DModel.cpp
@@ -25,7 +25,7 @@ ProcessModel::ProcessModel(
     m_startState{new ProcessState{*this, 0., this}},
     m_endState{new ProcessState{*this, 1., this}}
 {
-    metadata().setName(QString("Autom3D.%1").arg(this->id().val()));
+    metadata().setInstanceName(*this);
     m_handles.emplace_back(-0.5, -0.5, -0.5);
     m_handles.emplace_back(0, 0, 0);
     m_handles.emplace_back(0.5, 0.5, 0.5);
@@ -43,7 +43,7 @@ ProcessModel::ProcessModel(
     m_startState{new ProcessState{*this, 0., this}},
     m_endState{new ProcessState{*this, 1., this}}
 {
-    metadata().setName(QString("Autom3D.%1").arg(this->id().val()));
+    metadata().setInstanceName(*this);
 }
 
 QString ProcessModel::prettyName() const
